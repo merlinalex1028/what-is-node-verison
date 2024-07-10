f287ffdc6252e9d3b212f5b4a22262368a789825
MerlinAlex <merlinalex@qq.com>
2024-07-10 19:37:43 +0800

    refactor: update status bar item in activate function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
-import { window } from 'vscode'
+import { exec } from 'node:child_process'
+import type { ExtensionContext } from 'vscode'
+import { StatusBarAlignment, window } from 'vscode'
 
-export function activate() {
-  window.showInformationMessage('Hello')
+function getNodeVersion(): Promise<string> {
+  return new Promise((resolve) => {
+    exec('node -v', (error, stdout) => {
+      if (error) {
+        console.error(`exec error: ${error}`)
+        return
+      }
+
+      resolve(`\$(tools) ${stdout}`)
+    })
+  })
+}
+
+export function activate(context: ExtensionContext) {
+  let statusBarName = ''
+  let statusBarColor: string | undefined
+
+  const statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left, 100000)
+
+  async function updateStatusBarItem() {
+    statusBarName = await getNodeVersion()
+    statusBarColor = '#297583'
+    statusBarItem.text = statusBarName
+    statusBarItem.color = statusBarColor
+    statusBarItem.command = 'workbench.action.quickSwitchWindow'
+    statusBarItem.tooltip = 'Node Version'
+    statusBarItem.show()
+  }
+
+  context.subscriptions.push(statusBarItem)
+
+  updateStatusBarItem()
 }
 
 export function deactivate() {
