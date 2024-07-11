cafd7a7a86fe3ae204355c4045c1a3b6ec5f4e82
MerlinAlex <merlinalex@qq.com>
2024-07-11 10:52:29 +0800

    feat: add interval to update status bar item in activate function

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,10 @@ export function activate(context: ExtensionContext) {
   context.subscriptions.push(statusBarItem)
 
   updateStatusBarItem()
+
+  setInterval(() => {
+    updateStatusBarItem()
+  }, 5000)
 }
 
 export function deactivate() {
